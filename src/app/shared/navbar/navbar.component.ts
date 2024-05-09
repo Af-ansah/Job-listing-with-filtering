482ed4cb2165b1dbb4cc698874dbe0c818af5ade
Af-ansah <ansahgloria032@gmail.com>
2024-05-09 09:25:33 +0000

    feat:filtering - removeFilter and filtercard visibily

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { FilterComponent } from "../filter/filter.component";
+import { DataService } from '../../data.service';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css',
-    imports: [FilterComponent]
+    imports: [FilterComponent, CommonModule]
 })
 export class NavbarComponent {
+    isVisible = false;
+    visiblecard: Subscription;
+    constructor(private dataservice: DataService) {
+        this.visiblecard = this.dataservice.visibleSource.subscribe(visible => {
+        this.isVisible = visible;})
+      } 
+      ngOnDestroy() {
+        this.visiblecard.unsubscribe();
+      }
+ }
 
-}
